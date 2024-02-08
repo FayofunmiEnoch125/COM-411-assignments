2a90b5d35017245eec813c20e45b2e8130f8bd7d
Fayofunmi <75456687+FayofunmiEnoch125@users.noreply.github.com>
2024-02-08 14:13:54 +0100

    Add files via upload

---
+++ Exercise 1.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Com 411 Exercises</title>
+</head>
+<body>
+    <?php
+
+        $int = 7;
+        $str = "Fayofunmi";
+        $i = 0; 
+        $Double = 7.5;
+        $D = 2.5;
+
+        echo $int + $i ."\n";
+        echo $Double + $D ."\n";
+        echo "My name is ". $str;
+
+    ?>
+</body>
+</html>
